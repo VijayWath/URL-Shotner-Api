0fc86747e7d2d0b3bb8b9c60e21e6f5b212a6fd6
VijayWath] <126336500+VijayWath@users.noreply.github.com>
2024-01-20 20:31:25 +0530

    added parser

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import urlRoute from "./routers/url.js";
 import { connectToDb } from "./connections.js";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 2000;
@@ -8,6 +9,8 @@ const port = 2000;
 connectToDb("mongodb://127.0.0.1:27017/short-url").then(() => {
   console.log("DB connected");
 });
+
+app.use(bodyParser.urlencoded({extended : false}))
 
 app.use("/url", urlRoute);
 
