3f4baaaaf90c6ecbbe1dad1ba5a824c2a8d868df
VijayWath] <126336500+VijayWath@users.noreply.github.com>
2024-01-21 19:18:54 +0530

    Added serverside rendering

--- controllers/urlControllers.js
+++ controllers/urlControllers.js
@@ -3,13 +3,14 @@ import Url from "../Models/url.js";
 
 async function handelgeneratenewShorlUrl(req, res) {
   const shortId = nanoid(8);
-  if (!req.body.url) return res.status(400).json({ error: " url is required" });
+  if (!req.body.url)
+    return res.status(400).render("home", { error: " url is required" });
   await Url.create({
     shortId: shortId,
     redirectUrl: req.body.url,
     visitHistory: [],
   });
-  return res.status(200).json({ id: shortId });
+  return res.status(200).render("home", { id: shortId });
 }
 
 async function handeRedirectlUrl(req, res) {
--- index.js
+++ index.js
@@ -2,17 +2,27 @@ import express from "express";
 import urlRoute from "./routers/url.js";
 import { connectToDb } from "./connections.js";
 import bodyParser from "body-parser";
+import path from "path";
 
 const app = express();
-const port = 2000;
+const port = process.env.port | 3001;
+const password = encodeURIComponent("Omwath@2004");
+const DB = `mongodb+srv://vijaywath:${password}@cluster0.scee2et.mongodb.net/?retryWrites=true&w=majority`;
 
-connectToDb("mongodb://127.0.0.1:27017/short-url").then(() => {
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+connectToDb(DB).then(() => {
   console.log("DB connected");
 });
 
-app.use(bodyParser.urlencoded({extended : false}))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/url", urlRoute);
+
+app.get('/',(req,res)=>{
+  res.render('home');
+})
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
