f283c8aed88ee4908f9ce3b7e1b8e1e77654c403
VijayWath] <126336500+VijayWath@users.noreply.github.com>
2024-01-21 19:21:45 +0530

    Added serverside rendering

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ import urlRoute from "./routers/url.js";
 import { connectToDb } from "./connections.js";
 import bodyParser from "body-parser";
 import path from "path";
+import DB from "./secerets/secerates.js";
 
 const app = express();
 const port = process.env.port | 3001;
-const password = encodeURIComponent("Omwath@2004");
-const DB = `mongodb+srv://vijaywath:${password}@cluster0.scee2et.mongodb.net/?retryWrites=true&w=majority`;
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
@@ -20,9 +19,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/url", urlRoute);
 
-app.get('/',(req,res)=>{
-  res.render('home');
-})
+app.get("/", (req, res) => {
+  res.render("home");
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
