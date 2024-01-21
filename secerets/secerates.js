071a455c6d5d952dab6662b22ac1bbe2eac7e157
VijayWath] <126336500+VijayWath@users.noreply.github.com>
2024-01-21 19:23:36 +0530

    Added serverside rendering

--- secerets/secerates.js
+++ secerets/secerates.js
@@ -1,4 +1,4 @@
-const password = encodeURIComponent("Omwath@2004");
-const DB = `mongodb+srv://vijaywath:${password}@cluster0.scee2et.mongodb.net/?retryWrites=true&w=majority`;
+const password = encodeURIComponent("");
+const DB = ``;
 
 export default DB
